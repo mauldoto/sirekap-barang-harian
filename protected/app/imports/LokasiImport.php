92e6040555d5df4d869f06e57ef6c89ea58b0262
jpn <maulanaly97@gmail.com>
2024-07-12 11:05:01 +0700

    add karyawan and lokasi imports

---
+++ protected/app/imports/LokasiImport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Barang;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class LokasiImport implements ToCollection
+{
+    public function collection(Collection $rows)
+    {
+        DB::beginTransaction();
+
+        foreach ($rows as $index => $row) {
+            if ($index < 1) continue;
+            
+            $check = Barang::where('kode', $row[0])
+                ->orWhere('nama', $row[1])
+                ->first();
+
+            $newBarang = new Barang();
+            if ($check) {
+                $newBarang = $check;
+            }
+
+            $newBarang->kode = $row[0] ? $row[0] : generateReference('B');
+            $newBarang->nama = $row[1];
+            $newBarang->deskripsi = $row[3];
+            $newBarang->satuan = $row[2];
+            $newBarang->input_by = Auth::user()->id;
+
+            $newBarang->save();
+        }
+
+        DB::commit();
+    }
+}
