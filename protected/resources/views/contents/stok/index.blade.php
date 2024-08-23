0a9d8642c7ca28fb90d9d22ee9be4008fe28972b
jpn <maulanaly97@gmail.com>
2024-08-23 14:20:22 +0700

    hide button export

--- protected/resources/views/contents/stok/index.blade.php
+++ protected/resources/views/contents/stok/index.blade.php
@@ -20,7 +20,7 @@
                         <a href="{{route('stok.keluar.view')}}" class="btn btn-sm btn-warning"><i class='bx bx-archive-out'></i> Stok Keluar</a>
                         <a href="{{route('stok.log')}}" class="btn btn-sm btn-primary"><i class='bx bx-search-alt-2'></i> Cek Log</a>
                         <a href="{{route('stok.rencana')}}" class="btn btn-sm btn-secondary"><i class='bx bx-print-alt-2'></i> Lembar Stok</a>
-                        <a class="btn btn-sm btn-danger exportpdf-modal-btn"><i class='bx bx-archive-out'></i> Export PDF</a>
+                        {{-- <a class="btn btn-sm btn-danger exportpdf-modal-btn"><i class='bx bx-archive-out'></i> Export PDF</a> --}}
                     </div>
                 </div>
 
