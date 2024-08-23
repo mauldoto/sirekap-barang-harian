e58ceb18cc5b9517f0c90085cc99b84b05b439be
jpn <maulanaly97@gmail.com>
2024-08-23 14:17:42 +0700

    handle action button activity

--- protected/resources/views/contents/aktivitas/index.blade.php
+++ protected/resources/views/contents/aktivitas/index.blade.php
@@ -82,10 +82,12 @@
                                         <a class="dropdown-item detail-btn d-flex align-items-center" href="#" data-url="" data-id="{{$i->id}}"><i class='bx bx-search-alt-2 me-1'></i> Detail</a>
                                         <div class="dropdown-divider"></div>
                                         <a class="dropdown-item d-flex align-items-center" href="{{route('aktivitas.print.tiket', $i->no_referensi)}}" target="_blank" data-url=""><i class='bx bxs-discount me-1'></i> Print Tiket</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item delete-btn d-flex align-items-center" href="#" data-url=""><i class='bx bx-task me-1'></i> Update Status</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item delete-btn d-flex align-items-center" href="#" data-url=""><i class='bx bxs-edit me-1' ></i> Edit</a>
+                                        @if (!in_array($i->status, ['done', 'cancel']))
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item d-flex align-items-center update-status-btn" href="#" data-url="{{route('aktivitas.update.status', $i->no_referensi)}}" data-status="{{$i->status}}"><i class='bx bx-task me-1'></i> Update Status</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item d-flex align-items-center" href="{{route('aktivitas.edit', $i->no_referensi)}}" data-url=""><i class='bx bxs-edit me-1' ></i> Edit</a>
+                                        @endif
                                     </div>
                                 </div>
                             </td>
@@ -118,6 +120,16 @@
                             <td>Tanggal</td>
                             <td>:</td>
                             <td class="tanggal"></td>
+                        </tr>
+                        <tr>
+                            <td>Lokasi</td>
+                            <td>:</td>
+                            <td class="lokasi"></td>
+                        </tr>
+                        <tr>
+                            <td>Sublokasi</td>
+                            <td>:</td>
+                            <td class="sublokasi"></td>
                         </tr>
                         <tr>
                             <td>Teknisi</td>
@@ -171,6 +183,57 @@
     </div><!-- /.modal-dialog -->
 </div>
 
+<div id="modalUpdateStatus" class="modal fade" tabindex="-1" aria-labelledby="modalUpdateStatusLabel" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalUpdateStatusLabel">Update Status Aktivitas</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="modal-form" id="formUpdateStatus" action="" method="post">
+                @csrf
+                @method('put')
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="mb-3 col-3">
+                            <input class="form-check-input" type="radio" name="status" id="waiting" value="waiting">
+                            <label class="form-check-label" for="waiting">
+                              <span class="p-1 rounded text-white bg-secondary">Waiting</span>
+                            </label>
+                        </div>
+                        <div class="mb-3 col-3">
+                            <input class="form-check-input" type="radio" name="status" id="progress" value="progress">
+                            <label class="form-check-label" for="progress">
+                                <span class="p-1 rounded text-white bg-warning">Progress</span>
+                            </label>
+                        </div>
+                        <div class="mb-3 col-3">
+                            <input class="form-check-input" type="radio" name="status" id="done" value="done">
+                            <label class="form-check-label" for="done">
+                                <span class="p-1 rounded text-white bg-success">Done</span>
+                            </label>
+                        </div>
+                        <div class="mb-3 col-3">
+                            <input class="form-check-input" type="radio" name="status" id="cancel" value="cancel">
+                            <label class="form-check-label" for="cancel">
+                                <span class="p-1 rounded text-white bg-danger">Cancel</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <label class="form-label">Deskripsi</label>
+                        <textarea class="form-control edit-deskripsi" name="deskripsi" cols="30" rows="5" disabled></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary waves-effect" data-bs-dismiss="modal">Tutup</button>
+                    <button type="button" class="btn btn-primary waves-effect waves-light btn-submit-u">Simpan</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div>
+
 @endsection
 
 @section('css')
@@ -204,7 +267,10 @@
                 if (!res.status) return
 
                 $('.ka').html(res.data.no_referensi)
-                $('.tanggal').html(res.data.tanggal_berangkat + ' - ' +res.data.tanggal_pulang)
+                $('.lokasi').html(res.data.lokasi.nama)
+                $('.sublokasi').html(res.data.sublokasi.nama)
+                $('.ka').html(res.data.no_referensi)
+                $('.tanggal').html(res.data.tanggal_berangkat + ' / ' +res.data.tanggal_pulang)
 
                 let tempTeknisi = ''
                 for (const teknisi of res.data.teknisi) {
@@ -226,6 +292,30 @@
             })
         }
 
+        document.querySelectorAll('input[type="radio"][name="status"]').forEach(element => {
+            element.addEventListener('change', function(e){
+                if (e.target.checked && e.target.value == 'cancel') {
+                    document.querySelector('.edit-deskripsi').disabled = false;
+                } else {
+                    document.querySelector('.edit-deskripsi').disabled = true;
+                }
+            })
+        });
+
+        function getDetailStatus(status) {
+
+            let radioStatus = document.querySelectorAll('input[type="radio"][name="status"]');
+            radioStatus.forEach(elm => {
+                elm.checked = false
+            });
+
+            document.getElementById(status).checked = true
+
+            setTimeout(() => {
+                showModalUpdate();
+            }, 500);
+        }
+
         function showModalAktivitas() {
             const myModal = new bootstrap.Modal('#modalDetail', {
                 show: true
@@ -233,8 +323,39 @@
             myModal.show()
         }
 
+        function showModalUpdate() {
+            const myModal = new bootstrap.Modal('#modalUpdateStatus', {
+                show: true
+            })
+            myModal.show()
+        }
+
         $('#datatable-aktivitas').on('click', '.detail-btn', function() {
             getDetailAktivitas($(this).data('id'))
+        })
+
+        $('#datatable-aktivitas').on('click', '.update-status-btn', function() {
+            getDetailStatus($(this).data('status'))
+
+            const url = $(this).data("url");
+            const form = $("#formUpdateStatus").attr("action", url);
+        })
+
+        $('.btn-submit-u').on('click', function() {
+            Swal.fire({
+                title: "Apakah anda yakin?"
+                , text: "Status aktivitas akan diupdate!"
+                , icon: "warning"
+                , showCancelButton: true
+                , confirmButtonColor: "#3085d6"
+                , cancelButtonColor: "#d33"
+                , confirmButtonText: "Ya, Update!"
+                , cancelButtonText: "Batalkan"
+            , }).then((result) => {
+                if (result.isConfirmed) {
+                    $('#formUpdateStatus')[0].submit();
+                }
+            });
         })
 
         $("#datatable-aktivitas").on("click", ".delete-btn", function() {
