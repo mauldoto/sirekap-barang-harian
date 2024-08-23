42401995c0285a0918ec1217de1e47d4b543e616
jpn <maulanaly97@gmail.com>
2024-08-23 14:17:05 +0700

    adding function for update status and edit activity

--- protected/app/Http/Controllers/AktivitasController.php
+++ protected/app/Http/Controllers/AktivitasController.php
@@ -40,7 +40,7 @@ class AktivitasController extends Controller
 
     public function getDetail(request $request, $id)
     {
-        $aktivitas = Aktivitas::where('id', $id)->with('teknisi.karyawan')->first();
+        $aktivitas = Aktivitas::where('id', $id)->with(['teknisi.karyawan', 'lokasi', 'sublokasi'])->first();
         $barang = Stok::where('type', 'keluar')->where('id_aktivitas', $aktivitas->id)->with('stok.barang')->first();
 
         $aktivitas->barang = $barang ? $barang->stok : [];
@@ -154,6 +154,117 @@ class AktivitasController extends Controller
 
         DB::commit();
         return back()->with(['success' => 'Input aktivitas berhasil.']);
+    }
+
+    public function edit($tiket)
+    {
+        $karyawan = Karyawan::all();
+        $lokasi = Lokasi::all();
+        $aktivitas = Aktivitas::where('no_referensi', $tiket)->with(['teknisi.karyawan', 'lokasi', 'sublokasi'])->first();
+        if (!$aktivitas) {
+            return back()->withErrors(['Aktivitas dengan no tiket ' .$tiket. ' tidak ditemukan.']);
+        }
+        
+        return view('contents.aktivitas.edit', compact('karyawan', 'lokasi', 'aktivitas'));
+    }
+
+    public function update(Request $request, $tiket)
+    {
+        $validator = Validator::make($request->all(), [
+            'lokasi' => 'required',
+            'sublokasi' => 'required',
+            'teknisi' => 'required|array',
+            'teknisi.*' => 'required',
+            'deskripsi' => 'nullable',
+            'tanggal_berangkat' => 'required|date',
+            'tanggal_pulang' => 'required|date',
+            'status' => 'nullable|in:waiting,progress,done,cancel',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $startDate = Carbon::createFromFormat('Y-m-d', $request->tanggal_berangkat);
+        $endDate = Carbon::createFromFormat('Y-m-d', $request->tanggal_pulang);
+
+        if ($startDate > $endDate) {
+            return back()->withErrors(['Tanggal berangkat tidak boleh lebih dari tanggal pulang.'])->withInput();
+        }
+
+        DB::beginTransaction();
+
+        $activity = Aktivitas::where('no_referensi', $tiket)->first();
+        if (!$activity) {
+            return redirect(route('aktivitas.index'))->withErrors(['Aktivitas dengan no tiket ' .$tiket. ' tidak ditemukan.']);
+        }
+
+        $activity->id_lokasi = $request->lokasi;
+        $activity->id_sub_lokasi = $request->sublokasi;
+        $activity->tanggal_berangkat = $request->tanggal_berangkat;
+        $activity->tanggal_pulang = $request->tanggal_pulang;
+        $activity->deskripsi = $request->deskripsi;
+        $activity->input_by = $request->user()->id;
+
+        if (!$activity->save()) {
+            DB::rollBack();
+            return back()->withErrors(['Update data aktivitas gagal.'])->withInput();
+        }
+
+        AktivitasKaryawan::where('id_aktivitas', $activity->id)->delete();
+
+        foreach ($request->teknisi as $key => $teknisi) {
+            $checkTeknisi = Karyawan::where('id', $teknisi)->first();
+            if (!$checkTeknisi) {
+                DB::rollBack();
+                return back()->withErrors(['Update data teknisi ke aktivitas gagal, data teknisi tidak ditemukan.'])->withInput();
+            }
+
+            $inpTeknisi = new AktivitasKaryawan();
+            $inpTeknisi->id_aktivitas = $activity->id;
+            $inpTeknisi->id_karyawan = $checkTeknisi->id;
+
+            if (!$inpTeknisi->save()) {
+                DB::rollBack();
+                return back()->withErrors(['Update data teknisi ke aktivitas gagal.'])->withInput();
+            }
+        }
+
+        DB::commit();
+        return back()->with(['success' => 'Update aktivitas berhasil.']);
+    }
+
+    public function updateStatus(Request $request, $tiket)
+    {
+        $validator = Validator::make($request->all(), [
+            'status' => 'required|in:waiting,progress,done,cancel',
+            'deskripsi' => 'nullable',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $activity = Aktivitas::where('no_referensi', $tiket)->first();
+        if (!$activity) {
+            return redirect(route('aktivitas.index'))->withErrors(['Aktivitas dengan no tiket ' .$tiket. ' tidak ditemukan.']);
+        }
+
+        if (in_array($activity->status, ['done', 'cancel'])) {
+            return back()->withErrors(['Aktivitas sudah berstatus DONE dan CANCEL tidak dapat diupdate.']);
+        }
+
+        $activity->status = $request->status;
+
+        if (!$activity->save()) {
+            return back()->withErrors(['Update status aktivitas gagal.'])->withInput();
+        }
+
+        return back()->with(['success' => 'Update Status aktivitas ' . $tiket . ' menjadi ' . strtoupper($request->status) .' berhasil']);
     }
 
     public function exportPdf(Request $request)
