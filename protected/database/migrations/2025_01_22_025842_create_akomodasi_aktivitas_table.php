d7099c8a5fe292fc215506be6f0a068ee5fa47d7
jpn <maulanaly97@gmail.com>
2025-01-23 21:59:35 +0700

    edit migration pivot table accomodation

--- protected/database/migrations/2025_01_22_025842_create_akomodasi_aktivitas_table.php
+++ protected/database/migrations/2025_01_22_025842_create_akomodasi_aktivitas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('akomodasi_aktivitas', function (Blueprint $table) {
             $table->unsignedBigInteger('id_akomodasi');
             $table->unsignedBigInteger('id_aktivitas');
+            $table->timestamps();
         });
     }
 
