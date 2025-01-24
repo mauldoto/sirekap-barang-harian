0903fd602f92ab439e1253636978feeebc8ba335
jpn <maulanaly97@gmail.com>
2025-01-24 16:25:03 +0700

    add func to update accomodation

--- protected/app/Http/Controllers/AkomodasiController.php
+++ protected/app/Http/Controllers/AkomodasiController.php
@@ -108,4 +108,75 @@ class AkomodasiController extends Controller
         DB::commit();
         return back()->with(['success' => 'Input akomodasi berhasil.']);
     }
+
+    public function editView(Request $request, $noref)
+    {
+        $akomodasi = Akomodasi::where('no_referensi', $noref)->with(['aktivitas' => ['lokasi', 'sublokasi']])->first();
+
+        $aktivitas = Aktivitas::whereIn('status', ['waiting', 'progress'])
+            ->doesntHave('akomodasi')
+            ->with(['lokasi', 'sublokasi'])
+            ->get();
+
+        // dd($dateNow);
+        return view('contents.akomodasi.edit', compact('akomodasi', 'aktivitas'));
+    }
+
+    public function editAkomodasi(Request $request, $noref)
+    {
+        // validasi
+        $validator = Validator::make($request->all(), [
+            // 'noref'             => 'required',
+            'tanggal'           => 'required|date',
+            'nominal_pengajuan' => 'required|numeric',
+            'nominal_realisasi' => 'required|numeric',
+            'keterangan'        => 'nullable',
+            'aktivitas'         => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $akomodasi = Akomodasi::where('no_referensi', $noref)->first();
+        if (!$akomodasi) {
+            return back()->withErrors(['Data akomodasi tidak ditemukan atau sudah dihapus.']);
+        }
+
+        DB::beginTransaction();
+
+        $akomodasi->tanggal_terbit = $request->tanggal;
+        $akomodasi->nominal_pengajuan = $request->nominal_pengajuan;
+        $akomodasi->nominal_realisasi = $request->nominal_realisasi;
+        $akomodasi->id_pemohon = $request->pemohon;
+        $akomodasi->input_by = $request->user()->id;
+        $akomodasi->deskripsi = $request->keterangan;
+
+        if (!$akomodasi->save()) {
+            DB::rollback();
+            return back()->withErrors(['Gagal update data akomodasi.']);
+        }
+
+        $deleteAkm = LogAkomodasi::where('id_akomodasi', $akomodasi->id)->delete();
+        if (!$deleteAkm) {
+            DB::rollback();
+            return back()->withErrors(['Gagal update data akomodasi pada bagian link akomodasi dan aktivitas.']);
+        }
+
+        foreach ($request->aktivitas as $key => $aktv) {
+            $newRel = new LogAkomodasi();
+            $newRel->id_akomodasi = $akomodasi->id;
+            $newRel->id_aktivitas = $aktv;
+
+            if (!$newRel->save()) {
+                DB::rollback();
+                return back()->withErrors(['Gagal link akomodasi dan aktivitas.']);
+            }
+        }
+
+        DB::commit();
+        return back()->with(['success' => 'Update data akomodasi berhasil.']);
+    }
 }
